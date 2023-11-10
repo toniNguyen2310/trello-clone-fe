56b803340b526732446354bdabd2516b98a9e156
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-11-11 00:54:27 +0700

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import "./App.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    {/* // <React.StrictMode> */}
-    <App />
-    {/* </React.StrictMode> */}
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </>
 );
