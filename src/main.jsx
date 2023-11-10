eb7bedb7a5ce90e729cbbdd1040ca84eacd88f65
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-11-11 01:01:32 +0700

    update version react

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-
-import App from "./App.jsx";
+import ReactDOM from "react-dom";
 import "./App.scss";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
