b9e8838bffb37fe419ef3e47afb245138cef7fb8
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-11-11 18:04:26 +0700

    add handle logout

--- src/Components/HeaderTop/HeaderTop.jsx
+++ src/Components/HeaderTop/HeaderTop.jsx
@@ -2,11 +2,30 @@ import React, { useEffect, useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import { BiSolidUser } from "react-icons/bi";
 import { HiOutlineLogout } from "react-icons/hi";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import { callLogout } from "../../Service/service";
+import { message } from "antd";
+
 function HeaderTop(props) {
+  const navigate=useNavigate();
   const [menu, setMenu] = useState(false);
   const [user, setUser] = useState("");
+  //HANDLE LOGOUT
+  const handleLogout = async () => {
+    const res = await callLogout();
+    if (res && res.data) {
+      message.success("Đăng Xuất thành công");
+      localStorage.removeItem("user");
+      localStorage.removeItem("refresh_token");
+      localStorage.removeItem("access_token");
+      setUser("");
+      navigate("/");
+      return;
+    } else {
+      return;
+    }
+  };
 
   window.addEventListener("click", function (event) {
     if (!event.target.closest(`#account-title`)) {
@@ -46,7 +65,7 @@ function HeaderTop(props) {
                 <BiSolidUser />
                 Tài khoản
               </div>
-              <div className="drop-down-box">
+              <div className="drop-down-box" onClick={handleLogout}>
                 <HiOutlineLogout />
                 Đăng xuất
               </div>
